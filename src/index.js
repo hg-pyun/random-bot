86d7b3ec6d5573d4f9d95aa2d0174565c0401387
hg-pyun <phg2491@gmail.com>
2018-05-06 15:22:54 +0900

    change PORT to 5000, add bodyParser

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.listen(80, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(PORT, function () {
+    console.log(`Example app listening on port ${PORT}`);
 });
+
+
+
